6fe1dab28c36a82007360297748e8b4459a8e2ea
DiegoPertierraM <diegopmenendez@gmail.com>
2024-05-07 23:58:29 +0200

    Add login form styles

--- projects/uniteam/src/app/features/login/login.component.ts
+++ projects/uniteam/src/app/features/login/login.component.ts
@@ -3,24 +3,41 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RepoUsersService } from '../../core/services/repo.users.service';
 import { StateService } from '../../core/services/state.service';
 import { UserLoginDto } from '../../core/models/user.model';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'isdi-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
-  template: `<form [formGroup]="formLogin" (ngSubmit)="submit()">
-    <div>
-      <label for="user">Username / email</label>
-      <input id="user" type="text" formControlName="user" />
-    </div>
-    <div>
-      <label for="password">Password</label>
-      <input id="password" type="password" formControlName="password" />
-    </div>
+  imports: [ReactiveFormsModule, RouterModule],
+  template: `
+    <form [formGroup]="formLogin" (ngSubmit)="submit()">
+      <section>
+        <h1>
+          <img src="assets/img/logo-with-text.svg" alt="Logo de Uniteam" />
+        </h1>
+        <h2>Inicio de sesión</h2>
+        <p>
+          ¿Aún no tienes usuario? <a [routerLink]="'/register'">Regístrate</a>
+        </p>
+      </section>
+      <div>
+        <label for="user"><h3>Usuario / Email</h3></label>
+        <input id="user" type="text" formControlName="user" />
+      </div>
+      <div>
+        <label for="password"><h3>Contraseña</h3></label>
+        <input id="password" type="password" formControlName="password" />
+      </div>
 
-    <button type="submit" [disabled]="formLogin.invalid">Submit</button>
-  </form>`,
+      <button type="submit" [disabled]="formLogin.invalid">
+        Iniciar sesión
+      </button>
+      <div>
+        <input id="remember" type="checkbox" />
+        <label for="remember">Recuérdame</label>
+      </div>
+    </form>
+  `,
   styleUrl: './login.component.css',
 })
 export default class LoginComponent {
