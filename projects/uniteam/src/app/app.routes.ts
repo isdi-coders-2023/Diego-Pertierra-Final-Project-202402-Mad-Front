21aa85bcababecdb34c1cbc36b8f2bd5d92af890
DiegoPertierraM <diegopmenendez@gmail.com>
2024-05-20 09:38:52 +0200

    Deactivate guards

--- projects/uniteam/src/app/app.routes.ts
+++ projects/uniteam/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { loggedGuard } from './guards/logged.guard';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'landing' },
@@ -11,7 +10,6 @@ export const routes: Routes = [
   {
     path: 'home',
     title: 'Home',
-    canActivate: [loggedGuard],
     loadComponent: () => import('./features/home/home.component'),
   },
   {
@@ -27,32 +25,27 @@ export const routes: Routes = [
   {
     path: 'meets',
     title: 'Quedadas',
-    canActivate: [loggedGuard],
     loadComponent: () => import('./features/meets/meets.component'),
   },
   {
     path: 'meets/:id',
     title: 'Quedada',
-    canActivate: [loggedGuard],
     loadComponent: () =>
       import('./features/meet-details/meet-details.component'),
   },
   {
     path: 'profile',
     title: 'Perfil',
-    canActivate: [loggedGuard],
     loadComponent: () => import('./features/profile/profile.component'),
   },
   {
     path: 'create-meet',
     title: 'Nueva quedada',
-    canActivate: [loggedGuard],
     loadComponent: () => import('./features/create-meet/create-meet.component'),
   },
   {
     path: 'error',
     title: 'Error',
-    canActivate: [loggedGuard],
     loadComponent: () => import('./features/error/error.component'),
   },
   { path: '**', redirectTo: 'error' },
