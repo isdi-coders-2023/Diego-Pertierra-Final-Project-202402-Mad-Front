a0e70f25670ea1409baae5062fa1ffe22ab736cf
DiegoPertierraM <diegopmenendez@gmail.com>
2024-05-20 02:12:41 +0200

    Add logged guard to routes

--- projects/uniteam/src/app/guards/logged.guard.ts
+++ projects/uniteam/src/app/guards/logged.guard.ts
@@ -1,14 +1,16 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { StateService, State } from '../core/services/state.service';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const loggedGuard: CanActivateFn = (_route, _stateRoute) => {
   const stateSrv = inject(StateService);
+  const router = inject(Router);
   const userState: State = stateSrv.state;
 
   if (userState.loginState !== 'logged') {
     console.log('Guard block navigate: LoginState', userState.loginState);
+    router.navigate(['/login']);
     return false;
   }
 
